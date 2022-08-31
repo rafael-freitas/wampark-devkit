6ef30989dfdf40d74f05770089b8fcba80de5932
Rafael Freitas <rafael@tag.mx>
2022-08-31 03:33:47 -0300

    UPD [ui.routes] use database record of routes on select item on nav-list

--- routes/ui.routes.navlistLeft.onSelect.js
+++ routes/ui.routes.navlistLeft.onSelect.js
@@ -1,4 +1,5 @@
 import app from 'wampark'
+import Routes from '../db/models/routes/index.js'
 
 function truncate_with_ellipsis(s,maxLength) {
   if (s.length > maxLength) {
@@ -22,14 +23,25 @@ export default class UiComponent extends app.Route {
    * @param details
    */
   async endpoint (args = [], kwargs = {}, details = {}) {
-    console.log('ui.routes.navlistLeft.onSelect', args, kwargs, details)
+    // console.log('ui.routes.navlistLeft.onSelect', args, kwargs, details)
 
     const viewport = this.clientApplication.component('#viewport')
 
     const navlistLeft = this.clientApplication.component('#navlistLeft')
     const selected = await navlistLeft.method('getSelected')
 
-    viewport.method('setState', selected)
+    const doc = await Routes.findOne({_id: selected._id})
+
+    if (doc) {
+      viewport.method('setState', doc)
+    } else {
+      viewport.method('Message', {
+        type: 'warn',
+        message: `[WARN] Using selected content insted database record. Be careful!`
+      })
+      viewport.method('setState', selected)
+    }
+
     viewport.method('setWindowTitle', `${selected._id}`)
     
   }
