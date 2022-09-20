81b331fa3a30e68b02b1711d1a0e22b928b904af
Rafael Freitas <rafael@tag.mx>
2022-09-20 13:31:31 -0300

    UPD enable dev.env file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 import app from 'wampark'
-// enable .env file
-import dotenv from 'dotenv/config'
+import dotenv from 'dotenv'
 import db from './lib/db/index.js'
 import webserver from './lib/webserver/koa.js'
 import importRoute from './lib/importRoute.js'
@@ -17,49 +16,15 @@ import SystemUsers from './db/models/system_users/index.js'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
+// enable .env file
+dotenv.config({ path: '.env' })
+dotenv.config({ path: 'dev.env', override: true })
+
+
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const log = app.logger()
 
-// ----------- DATA BASE CONNECTION ----------
-if (process.env.DB_ENABLE) {
-  log.info(`DB is enabled ${log.colors.yellow(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)}`)
-  // montar connection string do mongoos
-  let mongodbConnectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-
-  // se tiver usuario e senha adicionar na string de conexão
-  if (process.env.DB_USER) {
-    mongodbConnectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-  }
-
-  db.connect(mongodbConnectionString)
-}
-
-// ----------- WAMP SERVER CONNECTION ----------
-
-process.nextTick(() => {
-  app.connectWampServer({
-    url: process.env.WAMP_URL,
-    realm: process.env.WAMP_REALM,
-    authid: process.env.WAMP_AUTHID,
-    authpass: process.env.WAMP_AUTHPASS,
-  })
-
-  app.on('wamp.session.start', async session => {
-    // esperar todos os modulos e rotas serem processados pra então iniciar o app
-    app.emit('app.start', session)
-  })
-})
-
-
-// ----------- WEBSERVER SETUP ----------
-
-if (process.env.HTTP_ENABLE) {
-  const HTTP_PORT = process.env.HTTP_PORT || 3000
-  webserver.listen(HTTP_PORT)
-  webserver.HTTP_ENABLE = true
-  log.info(`webserver listening on port ${log.colors.yellow(HTTP_PORT)}`)
-}
 
 export default app
 
@@ -90,11 +55,62 @@ export const lib = {
 export function pathTo (file) {
   return join(__dirname, '../', file)
 }
+
+/**
+ * 1- Connect to databse
+ * 2- Enable webserver
+ * 3- Connect to WAMP server nextTick
+ * 
+ */
+export function setup () {
+
+  // ----------- DATA BASE CONNECTION ----------
+  if (process.env.DB_ENABLE) {
+    log.info(`DB is enabled ${log.colors.yellow(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)}`)
+    // montar connection string do mongoos
+    let mongodbConnectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
+    // se tiver usuario e senha adicionar na string de conexão
+    if (process.env.DB_USER) {
+      mongodbConnectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+    }
+
+    db.connect(mongodbConnectionString)
+  }
+
+  // ----------- WAMP SERVER CONNECTION ----------
+
+  process.nextTick(() => {
+    app.connectWampServer({
+      url: process.env.WAMP_URL,
+      realm: process.env.WAMP_REALM,
+      authid: process.env.WAMP_AUTHID,
+      authpass: process.env.WAMP_AUTHPASS,
+    })
+
+    app.on('wamp.session.start', async session => {
+      // esperar todos os modulos e rotas serem processados pra então iniciar o app
+      app.emit('app.start', session)
+    })
+  })
+
+
+  // ----------- WEBSERVER SETUP ----------
+
+  if (process.env.HTTP_ENABLE) {
+    const HTTP_PORT = process.env.HTTP_PORT || 3000
+    webserver.listen(HTTP_PORT)
+    webserver.HTTP_ENABLE = true
+    log.info(`webserver listening on port ${log.colors.yellow(HTTP_PORT)}`)
+  }
+}
+
 /**
  * Init routes for application
  * @param {Array} routes Initial routes from config.routes.js
  */
 export function startEngine (routes, options = {}) {
+  setup()
   Object.assign(options, process.env, options)
   let executeRoutesEnabled = false
   if (!Array.isArray(routes.autoload)) {
